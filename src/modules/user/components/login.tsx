06421ad97499f72ae69ed4ddd4229b0afe41b604
Jhonatan Caicedo <jonatan9911@gmail.com>
2024-01-27 18:13:32 -0500

    feat(): add titles

--- src/modules/user/components/login.tsx
+++ src/modules/user/components/login.tsx
@@ -1,11 +1,25 @@
 import "./login.css";
+import portada from "../assets/portada.jpeg";
 
 export function Login() {
   return (
     <>
       <div className="login">
-        <div className="login-image">login</div>
-        <div className="login-form">Form</div>
+        <div className="login-image">
+          <div className="title">
+            <h1>ECG</h1>
+          </div>
+          <div className="image">
+            <img src={portada} alt="" />
+          </div>
+        </div>
+        <div className="login-form">
+          <div className="title">
+            <div>
+              <h1>Wellcome to ECG online</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
